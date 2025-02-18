5fe05fe1faa913e513b29ff3f7659a106b44c0c2
Pahrijal Saban Mubarok <kevinsudrajat27@gmail.com>
2025-02-19 02:10:32 +0700

    todos helper

---
+++ src/helper/TodosTableHelper.js
@@ -0,0 +1,32 @@
+const pool = require('../database/postgres/pool');
+
+const TodosTableHelper = {
+  async addTodo({
+    id = "todo-123",
+    title = "Hello",
+    description = "World",
+    due_date = "2099-08-08",
+    priority = "low",
+  }) {
+    const query = {
+      text: 'INSERT INTO todos VALUES($1, $2, $3, $4, $5) RETURNING id',
+      values: [id, title, description, due_date, priority],
+    };
+
+    await pool.query(query);
+  },
+  async findTodoById(id) {
+    const query = {
+      text: 'SELECT * FROM todos WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await pool.query(query);
+    return result.rows;
+  },
+  async cleanTable() {
+    await pool.query('DELETE FROM todos');
+  },
+};
+
+module.exports = TodosTableHelper;
