8367e7c23e6ff9282052b1a7776ad1196901edc0
Pahrijal Saban Mubarok <kevinsudrajat27@gmail.com>
2025-02-19 02:43:32 +0700

    change test

--- src/http/_test/todos.test.js
+++ src/http/_test/todos.test.js
@@ -1,7 +1,49 @@
 const pool = require('../../database/postgres/pool');
 const TodosTableTestHelper = require('../../helper/TodosTableHelper');
 const createServer = require('../createServer');
-
+const badPayload = [
+  {
+    title: 'new todo',
+    description: 'new description',
+    due_date: '2090-08-08',
+  },
+  {
+    title: '',
+    description: 'new description',
+    due_date: '2090-08-08',
+    priority: 'low',
+  },
+  {
+    title: 123,
+    description: 'new description',
+    due_date: '2090-08-08',
+    priority: 'low',
+  },
+  {
+    title: 'new todo',
+    description: 'new description',
+    due_date: '2000-08-08',
+    priority: 'low',
+  },
+  {
+    title: 'new todonew todonew todonew todonew todonew todonew todonew todonew todo',
+    description: 'new description',
+    due_date: '2000-08-08',
+    priority: 'low',
+  },
+  {
+    title: 'new todo',
+    description: 'new description',
+    due_date: '2090-08-08',
+    priority: 'end',
+  },
+  {
+    title: 'new todo',
+    description: 'new description',
+    due_date: 'invalid_date',
+    priority: 'low',
+  }
+]
 describe('when POST /todos', () => {
 
   beforeAll(async () => {
@@ -37,5 +79,19 @@ describe('when POST /todos', () => {
       expect(responseJson.data.todoId).toBeDefined();
       expect(typeof responseJson.data.todoId).toBe('string');
     });
+  });
+
+  it('should response 400 when add todo with bad payload', async () => {
+    const server = await createServer();
+    for (let i = 0; i < badPayload.length; i++) {
+      const response = await server.inject({
+        method: 'POST',
+        url: '/api/todos',
+        payload: badPayload[i],
+      });
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(400);
+      expect(typeof responseJson.errors).toBe('object');
+    }
   });
 });
