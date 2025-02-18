473928ec56fc4ba0763d03c5940d8953857db6b6
Pahrijal Saban Mubarok <kevinsudrajat27@gmail.com>
2025-02-19 03:45:19 +0700

    add get test

--- src/http/_test/todos.test.js
+++ src/http/_test/todos.test.js
@@ -44,7 +44,7 @@ const badPayload = [
     priority: 'low',
   }
 ]
-describe('when POST /todos', () => {
+describe('/api/todos enpoint', () => {
 
   beforeAll(async () => {
     await TodosTableTestHelper.cleanTable();
@@ -59,7 +59,7 @@ describe('when POST /todos', () => {
     await TodosTableTestHelper.cleanTable();
   });
 
-  describe('when POST /todos', () => {
+  describe('when POST /api/todos', () => {
     it('should response 201 and persisted todo', async () => {
       const requestPayload = {
         title: 'new todo',
@@ -79,19 +79,83 @@ describe('when POST /todos', () => {
       expect(responseJson.data.todoId).toBeDefined();
       expect(typeof responseJson.data.todoId).toBe('string');
     });
+    it('should response 400 when add todo with bad payload', async () => {
+      const server = await createServer();
+      for (let i = 0; i < badPayload.length; i++) {
+        const response = await server.inject({
+          method: 'POST',
+          url: '/api/todos',
+          payload: badPayload[i],
+        });
+        const responseJson = JSON.parse(response.payload);
+        expect(response.statusCode).toEqual(400);
+        expect(typeof responseJson.errors).toBe('object');
+      }
+    });
   });
-
-  it('should response 400 when add todo with bad payload', async () => {
-    const server = await createServer();
-    for (let i = 0; i < badPayload.length; i++) {
-      const response = await server.inject({
-        method: 'POST',
-        url: '/api/todos',
-        payload: badPayload[i],
+  describe('when GET /api/todos', () => {
+    it('should response 200 and return all todos', async () => {
+      const server = await createServer();
+      await TodosTableTestHelper.addTodo({
+        id: 'todo-123',
+        title: 'new todo',
+        description: 'new description',
+        due_date: '2090-08-08',
+        priority: 'low',
       });
+      await TodosTableTestHelper.addTodo({
+        id: 'todo-124',
+        title: 'new todo',
+        description: 'new description',
+        due_date: '2090-08-08',
+        priority: 'low',
+      });
+      const response = await server.inject({
+        method: 'GET',
+        url: '/api/todos',
+      });
+
       const responseJson = JSON.parse(response.payload);
-      expect(response.statusCode).toEqual(400);
-      expect(typeof responseJson.errors).toBe('object');
-    }
+      expect(response.statusCode).toEqual(200);
+      expect(responseJson.data).toHaveLength(2);
+    });
+  });
+  describe('when GET /api/todos/{id}', () => {
+    it('should response 200 and return todo by id', async () => {
+      const server = await createServer();
+      await TodosTableTestHelper.addTodo({
+        id: 'todo-123',
+        title: 'new todo',
+        description: 'new description',
+        due_date: '2090-08-08',
+        priority: 'low',
+      });
+      await TodosTableTestHelper.addTodo({
+        id: 'todo-124',
+        title: 'new todo',
+        description: 'new description',
+        due_date: '2090-08-08',
+        priority: 'low',
+      });
+      const response = await server.inject({
+        method: 'GET',
+        url: '/api/todos/todo-123',
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(200);
+      expect(responseJson.data.todo).toBeDefined();
+    });
+    it('should response 404 when todo not found', async () => {
+      const server = await createServer();
+      const response = await server.inject({
+        method: 'GET',
+        url: '/api/todos/todo-123',
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(404);
+      expect(responseJson.message).toEqual('Todo not found');
+    });
   });
 });
